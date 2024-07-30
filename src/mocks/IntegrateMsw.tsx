cf68ca35a43e631a00381c9f05920b603e71973e
eunhak <akfmzh1207@naver.com>
2024-07-30 22:10:17 +0900

    fix: mocking setting

--- src/mocks/IntegrateMsw.tsx
+++ src/mocks/IntegrateMsw.tsx
@@ -2,7 +2,11 @@
 import React, { useEffect } from 'react';
 
 async function startClientMSW() {
-  if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
+  if (
+    typeof window !== 'undefined' &&
+    process.env.NODE_ENV === 'development' &&
+    process.env.NEXT_PUBLIC_API_MOCKING === 'enable'
+  ) {
     const { worker } = await import('./browser');
     worker.start({
       onUnhandledRequest: 'bypass'
