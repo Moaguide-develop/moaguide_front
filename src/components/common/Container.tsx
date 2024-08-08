c4c2b7135a7435026536841047fad0884248e6b0
eunhak <akfmzh1207@naver.com>
2024-08-08 16:08:13 +0900

    fix: Container width fix

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -1,8 +1,8 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode } from 'react';
 
 const Container = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="max-w-[360px] w-full mx-auto desk:max-w-[1000px] desk:min-w-[400px]">
+    <div className="max-w-[360px] w-full mx-auto desk:max-w-[1900px] desk:min-w-[400px]">
       {children}
     </div>
   );
