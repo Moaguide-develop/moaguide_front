2ce4f07a0bec0d4f50c6835c2ecbc0b22eab97f0
eunhak <akfmzh1207@naver.com>
2024-08-21 21:12:38 +0900

    feat: ProductDetail UI

--- src/components/product/detail/ProductDetail.tsx
+++ src/components/product/detail/ProductDetail.tsx
@@ -28,7 +28,7 @@ const ProductDetail = () => {
   //   landElevation: '평지',
   //   landShape: '가장형',
   //   zoningNational:
-  //     '도시지역,일반상업지역,지구단위계획구역,도로(접합),중로2류(폭 15m~20m)(2016-12-01)(접합)',
+  //     '도시지역,일반상업지역,지구단위계획구역,도로(접합),중로2류(폭 80m~20m)(2016-12-01)(접합)',
   //   zoningOther:
   //     '교육환경보호구역(남부교육청에 반드시 확인요망)<교육환경 보호에 관한 법률>,대공방어협조구역(위탁고도:해발165m(지반+건축+옥탑 등), 육군수도방위사령부(02-524-3146)관할)<군사기지 및 군사시설 보호법>,과밀억제권역<수도권정비계획법>',
   //   latitude: 37.509421,
@@ -54,6 +54,46 @@ const ProductDetail = () => {
 
       <div className=" text-lg font-bold mb-[20px]">상권 공실률</div>
       <CommercialVacancyRateChart />
+      <div className=" text-lg font-bold mb-[20px]">접근성</div>
+      <div className="text-gray-400 mb-[10px]"> 주요 업무 지구</div>
+
+      <div className=" bg-gray-50 rounded-xl flex flex-col">
+        <div className="flex mb-[40px]  justify-center items-center mt-[20px]">
+          <div className=" "></div>
+          <div className="text-gray-200 ml-[120px]">가까운 역까지</div>
+          <div className="text-gray-200 ml-[120px]">차량으로</div>
+          <div className="text-gray-200 ml-[120px]">대중교통으로</div>
+        </div>
+
+        <div className="flex mb-[40px] justify-center items-center ">
+          <div className="text-base">CBD(도심권역)</div>
+          <div className="text-blue-500 ml-[120px]">약 80km</div>
+          <div className="text-blue-500 ml-[120px]">평균 30분 소요</div>
+          <div className="text-blue-500 ml-[120px]">평균 10분 소요</div>
+        </div>
+        <div className="flex mb-[40px]  justify-center items-center">
+          <div className="text-base">CBD(도심권역)</div>
+          <div className="text-blue-500 ml-[120px]">약 80km</div>
+          <div className="text-blue-500 ml-[120px]">평균 30분 소요</div>
+          <div className="text-blue-500 ml-[120px]">평균 10분 소요</div>
+        </div>
+        <div className="flex mb-[40px] justify-center items-center ">
+          <div className="text-base">CBD(도심권역)</div>
+          <div className="text-blue-500 ml-[120px]">약 80km</div>
+          <div className="text-blue-500 ml-[120px]">평균 30분 소요</div>
+          <div className="text-blue-500 ml-[120px]">평균 10분 소요</div>
+        </div>
+      </div>
+      <div className="text-gray-400 mb-[10px]"> 0.5km 이내 대중교통</div>
+
+      <div className="flex max-w-[1000px] w-full mx-auto  h-[120px]">
+        <div className="bg-gray-50 rounded-xl mr-[20px] flex flex-col w-[calc(50%-10px)] p-[20px] ">
+          <div className="text-base font-bold ">주변 지하철</div>
+        </div>
+        <div className="bg-gray-50 rounded-xl  w-[calc(50%-10px)]">
+          <div className="text-base font-bold ">주변 버스정류장</div>
+        </div>
+      </div>
 
       <div className=" text-lg font-bold mb-[20px]">유동인구</div>
       <FloatingPopulationChart />
