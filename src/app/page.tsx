2117dd60735d8df4057fa55842b70a0d92a49eea
eunhak <akfmzh1207@naver.com>
2024-08-21 21:39:16 +0900

    fix: Navbar형식 수정 + RSC 방식 query-string 라우팅 도입

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,38 @@
 import Navbar from '@/components/common/Navbar';
 import HomeIndex from '@/components/home/Index';
+import RecentlyIssueIndex from '@/components/recentlyIssue/Index';
 
 // import { useNavStore } from '@/store/nav.store';
 import React from 'react';
+import ProductPage from './product/page';
+import { SummaryData } from '@/types/Diviend';
+import Product from '@/components/product/Product';
+import ReportIndex from '@/components/report/ReportIndex';
 
-const HomePage = () => {
-  // const { currentNav } = useNavStore();
+const HomePage = async ({
+  searchParams
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) => {
+  const response = await fetch(`https://api.moaguide.com/summary/recent/building`, {
+    cache: 'no-store'
+  });
+
+  const data: SummaryData = await response.json();
+  console.log(searchParams['category']);
   return (
     <div>
       <Navbar />
-      <HomeIndex />
+
+      {searchParams['category'] === 'newissue' ? (
+        <RecentlyIssueIndex />
+      ) : searchParams['category'] === 'product' ? (
+        <Product divide={data.divide} summary={data.summary} />
+      ) : searchParams['category'] === 'report' ? (
+        <ReportIndex />
+      ) : (
+        <HomeIndex />
+      )}
     </div>
   );
 };
