e1ba7309f6db2ab04bc080aa445a01390059585c
hojin <devbang5674@gmail.com>
2024-08-27 19:34:31 +0900

    feat: search 2차 레이아웃

--- src/components/search/SearchIndex.tsx
+++ src/components/search/SearchIndex.tsx
@@ -1,27 +1,62 @@
-import React from 'react';
+'use client';
+import React, { ChangeEvent, useCallback, useRef, useState } from 'react';
 import SearchRank from './SearchRank';
 
 const SearchIndex = () => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [keyWord, setKeyWord] = useState('');
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleKeyword = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setKeyWord(e.target.value);
+  }, []);
+
+  const handleClear = useCallback(() => {
+    setKeyWord('');
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
   return (
     <div className="pt-10 flex flex-col  gap-10">
-      <div className="text-heading1 text-gray700 flex items-center justify-center">
-        찾으시는 상품이 있으신가요?
-      </div>
+      {keyWord ? null : (
+        <div className="text-heading1 text-gray700 flex items-center justify-center">
+          찾으시는 상품이 있으신가요?
+        </div>
+      )}
+
       <div className="flex items-center justify-center">
-        <div className="max-w-[520px] w-full bg-white flex items-center justify-center py-[14px] px-4 rounded-[12px] border-[1.5px] border-gray100">
+        <div
+          className={`max-w-[520px] w-full bg-white flex items-center justify-center py-[14px] px-4 rounded-[12px] 
+          ${isFocused ? 'border-[1.5px] border-normal' : 'border-[1.5px] border-gray100'}
+          `}>
           <input
-            type="search"
+            ref={inputRef}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+            value={keyWord}
+            onChange={handleKeyword}
+            type="text"
             placeholder="조각투자 상품 검색"
             className="w-full outline-none text-body2 "
           />
-          <img src="/images/search/search_icon.svg" alt="" />
+          {keyWord && (
+            <div onClick={handleClear} className="cursor-pointer mr-3">
+              <img src="/images/search/xcircle.svg" alt="" />
+            </div>
+          )}
+          <div className="cursor-pointer">
+            <img src="/images/search/search_icon.svg" alt="" />
+          </div>
         </div>
       </div>
-
-      <div>
-        <div className="text-heading3 text-gray700">실시간 검색</div>
-        <SearchRank />
-      </div>
+      {keyWord ? null : (
+        <div>
+          <div className="text-heading3 text-gray700">실시간 검색</div>
+          <SearchRank />
+        </div>
+      )}
     </div>
   );
 };
