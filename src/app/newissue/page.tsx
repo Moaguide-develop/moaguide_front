59cb0131544a6c8e65b6e3508ddb6bfe144d5fb2
eunhak <akfmzh1207@naver.com>
2024-09-05 17:19:05 +0900

    fix: suspense 제거

--- src/app/newissue/page.tsx
+++ src/app/newissue/page.tsx
@@ -1,14 +1,12 @@
+'use clinet';
 import RecentlyIssueIndex from '@/components/recentlyIssue/Index';
 import Navbar from '@/components/common/Navbar';
-import { Suspense } from 'react';
 const NewIssuePage = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div>
-        <Navbar />
-        <RecentlyIssueIndex />
-      </div>
-    </Suspense>
+    <div>
+      <Navbar />
+      <RecentlyIssueIndex />
+    </div>
   );
 };
 
