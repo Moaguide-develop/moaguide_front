81ccd0df882cfbb35739a60a42394777121ba21e
eunhak <akfmzh1207@naver.com>
2024-10-04 17:45:20 +0900

    feat: 한우 차트 api 등록

--- src/components/product/detail/cow/CowPriceChart.tsx
+++ src/components/product/detail/cow/CowPriceChart.tsx
@@ -1,4 +1,5 @@
-import React, { useRef, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import { Line } from 'react-chartjs-2';
 import {
   Chart as ChartJS,
@@ -8,8 +9,12 @@ import {
   LineElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
+  ChartOptions
 } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { useRef, useState } from 'react';
+import { ICowProductPrice } from '@/types/CowProductType';
 
 ChartJS.register(
   CategoryScale,
@@ -18,56 +23,60 @@ ChartJS.register(
   LineElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
+  ChartDataLabels
 );
 
-// 각 기간에 해당하는 데이터의 타입 정의
-type TimePeriod = '3개월' | '6개월' | '1년';
-
 const CowPriceChart = () => {
+  const [filteringData, setFilteringData] = useState('averagePrice');
+  const [DateData, setDateData] = useState('1');
+  const handleFiltering = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilteringData(e.target.id);
+  };
+
+  const fetchData = async () => {
+    const response = await axios.get<ICowProductPrice>(
+      `https://api.moaguide.com/detail/hanwoo/sub/hanwooPrice?category=${filteringData}&month=${DateData}`
+    );
+    return response.data;
+  };
+
+  const {
+    data: HanwooData,
+    isLoading,
+    error
+  } = useQuery({
+    queryKey: ['HanwooChart', filteringData, DateData],
+    queryFn: fetchData
+  });
+
   const chartRef = useRef(null);
 
-  const [chartData, setChartData] = useState<TimePeriod>('1년');
+  const HanWooDate = HanwooData?.object.map((item) => item.day) || [];
 
-  const dataSets: Record<TimePeriod, { labels: string[]; data: number[] }> = {
-    '3개월': {
-      labels: ['2023-10', '2023-11', '2023-12'],
-      data: [4500, 3000, 3500]
-    },
-    '6개월': {
-      labels: ['2023-07', '2023-08', '2023-09', '2023-10', '2023-11', '2023-12'],
-      data: [4200, 3900, 3500, 4500, 3000, 3500]
-    },
-    '1년': {
-      labels: [
-        '2023-01',
-        '2023-02',
-        '2023-03',
-        '2023-04',
-        '2023-05',
-        '2023-06',
-        '2023-07',
-        '2023-08',
-        '2023-09',
-        '2023-10',
-        '2023-11',
-        '2023-12'
-      ],
-      data: [4000, 4300, 3700, 3500, 4200, 4000, 3900, 3500, 4200, 4500, 3000, 3500]
-    }
+  const HanwooCount = HanwooData?.object.map((item) => item.value) || [];
+  const sortedHanwooCount = [...HanwooCount].sort((a, b) => b - a);
+  const maxHanwooCount = sortedHanwooCount[0] || 0;
+  const averageHanwooCount =
+    HanwooCount.reduce((acc, val) => acc + val, 0) / HanwooCount.length || 0;
+  const newVariable = Math.floor(maxHanwooCount + averageHanwooCount);
+
+  const dataSets = {
+    labels: HanWooDate,
+    data: HanwooCount
   };
 
   const data = {
-    labels: dataSets[chartData].labels,
+    labels: dataSets.labels,
     datasets: [
       {
-        label: '주가',
-        data: dataSets[chartData].data,
+        label: '한우 가격',
+        data: dataSets.data,
         borderColor: '#8a4af3',
         backgroundColor: '#8a4af3',
         pointBackgroundColor: '#8a4af3',
         pointBorderColor: '#8a4af3',
-        pointRadius: 5,
+        pointRadius: 0,
         pointHoverRadius: 7,
         fill: false,
         datalabels: {
@@ -105,7 +114,7 @@ const CowPriceChart = () => {
       y: {
         display: true,
         beginAtZero: true,
-        max: 6000,
+        max: newVariable,
         grid: {
           display: false
         }
@@ -122,22 +131,69 @@ const CowPriceChart = () => {
   };
 
   return (
-    <>
-      <div className="mb-4  flex justify-end">
+    <div>
+      <section>
+        <input
+          type="radio"
+          id="averagePrice"
+          className=" mr-[5px] cursor-pointer"
+          checked={filteringData == 'averagePrice'}
+          onChange={handleFiltering}
+        />
+        <label htmlFor="averagePrice" className="mr-[10px]">
+          두당 평균 도매가격
+        </label>
+        <input
+          type="radio"
+          id="cattlePrice"
+          className=" mr-[5px] cursor-pointer"
+          checked={filteringData == 'cattlePrice'}
+          onChange={handleFiltering}
+        />
+        <label htmlFor="cattlePrice" className="mr-[10px]">
+          거세우 평균가격
+        </label>
+        <input
+          type="radio"
+          id="grade1Rate"
+          className=" mr-[5px] cursor-pointer"
+          checked={filteringData == 'grade1Rate'}
+          onChange={handleFiltering}
+        />
+        <label htmlFor="grade1Rate" className="mr-[10px]">
+          1등급 이상 출현율
+        </label>
+        <input
+          type="radio"
+          id="productionCost"
+          className=" mr-[5px] cursor-pointer"
+          checked={filteringData == 'productionCost'}
+          onChange={handleFiltering}
+        />
+        <label htmlFor="productionCost" className="mr-[10px]">
+          두당 생산비
+        </label>
+      </section>
+      <div className=" flex justify-end">
         <button
-          className={`w-[55px] mr-2 p-2  rounded-lg ${chartData === '3개월' ? 'bg-purple-500 text-white' : 'bg-gray-300  '}`}
-          onClick={() => setChartData('3개월')}>
-          3개월
-        </button>
-        <button
-          className={`w-[55px] mr-2 p-2 rounded-lg ${chartData === '6개월' ? 'bg-purple-500 text-white' : 'bg-gray-300 '}`}
-          onClick={() => setChartData('6개월')}>
-          6개월
-        </button>
-        <button
-          className={`w-[55px] p-2 rounded-lg ${chartData === '1년' ? 'bg-purple-500 text-white' : 'bg-gray-300 '}`}
-          onClick={() => setChartData('1년')}>
+          className={`w-[55px] mr-2 p-2  rounded-lg ${DateData === '1' ? 'bg-purple-500 text-white' : 'bg-gray-300  '}`}
+          onClick={() => setDateData('1')}>
           1년
+        </button>
+        <button
+          className={`w-[55px] mr-2 p-2 rounded-lg ${DateData === '3' ? 'bg-purple-500 text-white' : 'bg-gray-300 '}`}
+          onClick={() => setDateData('3')}>
+          3년
+        </button>
+        <button
+          className={`w-[55px] mr-2  p-2 rounded-lg ${DateData === '5' ? 'bg-purple-500 text-white' : 'bg-gray-300 '}`}
+          onClick={() => setDateData('5')}>
+          5년
+        </button>
+        <button
+          className={`w-[55px] p-2 rounded-lg ${DateData === '100' ? 'bg-purple-500 text-white' : 'bg-gray-300 '}`}
+          onClick={() => setDateData('100')}>
+          전체
         </button>
       </div>
       <div className="flex flex-col items-center justify-center h-full bg-gray-50 mb-[100px]">
@@ -145,7 +201,7 @@ const CowPriceChart = () => {
           <Line ref={chartRef} data={data} options={options} />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/components/product/detail/cow/CowProductDetail.tsx
+++ src/components/product/detail/cow/CowProductDetail.tsx
@@ -1,4 +1,6 @@
+// import CowMarketChart from './cowmarketChart';
 import CowPriceChart from './CowPriceChart';
+import CowMarketChart from './CowMarketChart';
 
 const CowProductDetail = ({ url }: { url: string }) => {
   console.log(url);
@@ -6,6 +8,9 @@ const CowProductDetail = ({ url }: { url: string }) => {
     <div className="max-w-[1000px] mx-auto mt-[32px]">
       <div className=" text-lg font-bold mb-[20px]">한우 가격</div>
       <CowPriceChart />
+
+      {/* <CowMarketChart /> */}
+      <CowMarketChart />
     </div>
   );
 };
