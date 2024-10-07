c6b0145bf075335eafe7c991f84577ff7087089c
jiohjung98 <ckaquz98@naver.com>
2024-10-07 16:41:08 +0900

    design: 메인페이지 반응형 디자인 작업

--- src/components/home/RealtimeRank.tsx
+++ src/components/home/RealtimeRank.tsx
@@ -17,9 +17,9 @@ const RealtimeRank = () => {
   }
 
   return (
-    <div className="max-w-[280px] w-full h-[291px] hidden desk2:block">
+    <div className="max-w-[280px] w-full h-[291px] hidden sm:block">
       <div className="flex flex-col gap-5">
-        <div className="mt-1 text-body5 text-black desk2:text-heading4">
+        <div className="mt-1 text-body5 text-black sm:text-heading4">
           실시간 검색 순위
         </div>
         <div className="p-5 shadow-custom-normal rounded-[12px] flex-1">
