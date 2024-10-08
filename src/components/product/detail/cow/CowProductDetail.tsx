7f30a82eedd09ff062e76ba05f34d20d4897e101
eunhak <akfmzh1207@naver.com>
2024-10-08 15:23:04 +0900

    fix: COW 차트 레이아웃 수정

--- src/components/product/detail/cow/CowProductDetail.tsx
+++ src/components/product/detail/cow/CowProductDetail.tsx
@@ -5,7 +5,7 @@ import CowMarketChart from './CowMarketChart';
 const CowProductDetail = ({ url }: { url: string }) => {
   return (
     <div className="max-w-[1000px] mx-auto mt-[32px]">
-      <div className=" text-lg font-bold mb-[20px]">한우 가격</div>
+      <div className=" text-2xl font-bold mb-[20px]">한우 가격</div>
       <CowPriceChart />
 
       {/* <CowMarketChart /> */}
