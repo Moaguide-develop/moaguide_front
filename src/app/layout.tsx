04bb59365b4f462eaa1daa003a6466bbf2a1d040
eunhak <akfmzh1207@naver.com>
2024-10-13 22:34:04 +0900

    feat: mobile 전용 gnb layout 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
     type: 'website',
     images: [
       {
-        url: '/favicon.svg', // Open Graph 이미지 URL
+        url: '/favicon.svg',
         width: 1200,
         height: 630,
         alt: '모아가이드 대표 이미지'
