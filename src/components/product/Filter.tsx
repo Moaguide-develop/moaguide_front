e64a9bef335836f4983b8ba8d783fb7ccc765649
eunhak <akfmzh1207@naver.com>
2024-10-14 17:36:36 +0900

    fix: sticky 추가

--- src/components/product/Filter.tsx
+++ src/components/product/Filter.tsx
@@ -26,7 +26,7 @@ const Filter = () => {
   };
 
   return (
-    <div className=" h-[85px] flex flex-row desk2:justify-center md:gap-20 my-[28px] desk:mx-[20px] desk:justify-start  desk:gap-4  overflow-x-scroll overflow-y-hidden scrollbar-hide">
+    <div className="bg-white sticky desk:top-[117px]  md:top-[105px] z-[99999] h-[143px] flex flex-row desk2:justify-center md:gap-20  items-center desk:justify-start  desk:gap-4  overflow-x-scroll overflow-y-hidden scrollbar-hide">
       {Object.keys(FilterElement).map((key) => (
         <div key={key} className="  cursor-pointer ">
           <div className=" w-[61px] flex flex-row justify-center items-center ">
