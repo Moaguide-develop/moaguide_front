a26bd093e6921bfffac717f8cac31e4be8fb78ab
jiohjung98 <ckaquz98@naver.com>
2024-10-18 15:15:17 +0900

    feat: 알림페이지 기능 구현 완료

--- src/components/alarm/AlarmItem.tsx
+++ src/components/alarm/AlarmItem.tsx
@@ -16,12 +16,22 @@ const AlarmItem = () => {
     isLoading 
   } = useNotifications(); 
 
-  console.log({
-    hasNextPage,
-    isFetching,
-    isFetchingNextPage,
-    isLoading
-  });
+  const [showSkeleton, setShowSkeleton] = useState(true);
+
+  useEffect(() => {
+    const hasVisited = sessionStorage.getItem('hasVisitedAlarmPage');
+
+    if (!hasVisited) {
+      const timer = setTimeout(() => {
+        setShowSkeleton(false); 
+        sessionStorage.setItem('hasVisitedAlarmPage', 'true'); 
+      }, 2000);
+
+      return () => clearTimeout(timer);
+    } else {
+      setShowSkeleton(false);
+    }
+  }, []);
 
   const loadMore = useCallback(
     _.throttle(() => {
@@ -46,57 +56,59 @@ const AlarmItem = () => {
       </div>
       <div className='h-4 bg-bg'>
       </div>
-        {isLoading  ? (
-          <div>
-            {Array.from({ length: 6 }).map((_, i) => (
-              <Skeleton key={i} />
-            ))}
-          </div>
-        ) : (
-          <Virtuoso
-            style={{ height: '100%', margin: '0px' }}
-            useWindowScroll
-            data={notifications}
-            totalCount={notifications.length}
-            endReached={loadMore} 
-            components={{
-              List: React.forwardRef(({ style, ...listProps }, ref) => (
-                <div
-                  {...listProps}
-                  ref={ref}
-                  style={{ ...style, backgroundColor: 'rgba(247, 248, 251, 0.8)' }}
-                />
-              )),
-            }}
-            itemContent={(index, notification) => (
-              <div 
-              key={notification.id} 
-              className="bg-white min-h-[109px] flex justify-between items-center p-4 shadow-md mb-4 mx-auto lg:max-w-[1000px] w-[90%] lg:w-[100%] border border-[#ECEFF2] rounded-[12px]"
-            >
-              <div className='flex flex-col justify-between h-full max-w-[80%]'> 
-                <div className="text-sm text-[#6E6F73] font-medium">
-                  관심 상품 업데이트
-                </div>
-                <div className="text-black text-md">
-                  {notification.message}
-                </div>
+      {isLoading || showSkeleton ? (
+        <div>
+          {Array.from({ length: 10 }).map((_, i) => (
+            <Skeleton key={i} />
+          ))}
+        </div>
+      ) : (
+        <Virtuoso
+          style={{ height: '100%', backgroundColor: 'rgba(247, 248, 251, 0.8)'  }}
+          useWindowScroll
+          data={notifications}
+          totalCount={notifications.length}
+          endReached={loadMore} 
+          components={{
+            List: React.forwardRef(({ style, ...listProps }, ref) => (
+              <div
+                {...listProps}
+                ref={ref}
+                style={{ ...style, backgroundColor: 'rgba(247, 248, 251, 0.8)' }}
+              />
+            )),
+          }}
+          itemContent={(index, notification) => (
+            <><div
+            key={notification.id}
+            className="bg-white min-h-[109px] flex justify-between items-center p-4 shadow-md mx-auto lg:max-w-[1000px] w-[90%] lg:w-[100%] border border-[#ECEFF2] rounded-[12px]"
+          >
+            <div className='flex flex-col justify-between h-full w-[80%] gap-5'>
+              <div className="text-sm text-[#6E6F73] font-medium">
+                관심 상품 업데이트
               </div>
-              <div className="min-w-[20px] mr-[10px] flex mb-auto text-[#A2A5AA] text-sm whitespace-nowrap">
-                {notification.date}
+              <div className="text-black text-md">
+                {notification.message}
               </div>
             </div>
-            )}
-          />
-        )}
-
-        {isFetchingNextPage && (
-          <div>
-            {Array.from({ length: 3 }).map((_, i) => (
-              <Skeleton key={i} />
-            ))}
+            <div className="min-w-[20px] pl-[10px] flex mb-auto text-[#A2A5AA] text-sm whitespace-nowrap">
+              {notification.date}
+            </div>
           </div>
-        )}
-        <div className='h-4 bg-bg'></div>
+            <div className='h-4'>
+            </div>
+          </>
+          )}
+        />
+      )}
+
+      {isFetchingNextPage && (
+        <div>
+          {Array.from({ length: 10 }).map((_, i) => (
+            <Skeleton key={i} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
