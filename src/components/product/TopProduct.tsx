92edb27ee578080cd57c9b14320e0071788b8fb5
eunhak <akfmzh1207@naver.com>
2024-10-27 21:06:41 +0900

    feat: 슬라이더 동적 재생 추가

--- src/components/product/TopProduct.tsx
+++ src/components/product/TopProduct.tsx
@@ -53,10 +53,10 @@ const TopProduct = memo(({ summary }: TopProductProps) => {
           spaceBetween={20}
           slidesPerView={2}
           pagination={{ el: '.swiper-pagination', clickable: true, enabled: false }}
-          // autoplay={{
-          //   delay: 5000,
-          //   disableOnInteraction: false // 사용자 상호작용시 슬라이더 일시 정지 비활성
-          // }}
+          autoplay={{
+            delay: 1000,
+            disableOnInteraction: false // 사용자 상호작용시 슬라이더 일시 정지 비활성
+          }}
           // navigation
           modules={[Navigation, Pagination]}
           breakpoints={{
