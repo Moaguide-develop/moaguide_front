1e82ae70c598c050390bf40c0b1d1a9d5f8ad5f1
jiohjung98 <ckaquz98@naver.com>
2024-10-29 01:09:53 +0900

    fix: 토큰 관리

--- src/service/change.ts
+++ src/service/change.ts
@@ -1,25 +1,35 @@
+import { removeToken } from "@/utils/localStorage";
 import { refreshAccessToken } from "./auth";
 import { axiosInstance } from "./axiosInstance";
 import { getCookie, setCookie, removeCookie } from '@/utils/cookie';
 
 export const updateNickname = async (nickname: string): Promise<boolean> => {
-    try {
-      const response = await axiosInstance.patch('/user/update/nickname', { nickname });
-  
-      if (response.status === 200) {
-        removeCookie('access_token');
-        removeCookie('refresh');
-        await refreshAccessToken();
+  try {
+    const response = await axiosInstance.patch('/user/update/nickname', { nickname });
+
+    if (response.status === 200) {
+      // 기존 토큰 삭제
+      removeToken();
+
+      // 새로운 토큰 발급 요청에 지연을 추가
+      try {
+        await new Promise(resolve => setTimeout(resolve, 100)); // 100ms 지연
+        await refreshAccessToken(); // refreshAccessToken이 새로운 access_token과 refresh 토큰을 저장합니다.
         return true;
-      } else {
-        console.error('닉네임 수정 실패:', response.status);
+      } catch (error) {
+        console.error('토큰 갱신 실패:', error);
+        alert('토큰 갱신에 실패했습니다. 다시 로그인해 주세요.');
         return false;
       }
-    } catch (error) {
-      console.error('API 호출 오류:', error);
+    } else {
+      console.error('닉네임 수정 실패:', response.status);
       return false;
     }
-  };
+  } catch (error) {
+    console.error('API 호출 오류:', error);
+    return false;
+  }
+};
 
 
 export const checkPassword = async (password: string): Promise<string> => {
