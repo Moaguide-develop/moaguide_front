220f6e331478ccbc04e9e21a30dbbd7f016c3264
eunhak <akfmzh1207@naver.com>
2024-10-28 14:00:14 +0900

    fix: robot.txt 수정

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/'
     },
-    sitemap: 'https://.../sitemap.xml'
+    sitemap: 'https://moaguide.com/sitemap.xml'
   };
 }
