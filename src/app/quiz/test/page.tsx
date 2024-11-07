4893bc294fae700ee16b6c76e2194ad5abece312
jiohjung98 <ckaquz98@naver.com>
2024-11-08 02:27:24 +0900

    design: 퀴즈 인풋

--- src/app/quiz/test/page.tsx
+++ src/app/quiz/test/page.tsx
@@ -83,7 +83,7 @@ const QuizTestPage = () => {
 
   return (
     <Container>
-      <div className="text-3xl font-bold mt-5">🎯 시험을 시작합니다 🎯</div>
+      <div className="text-center text-2xl font-bold mt-5">🎯 시험을 시작합니다 🎯</div>
 
       {showCountdown && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
@@ -94,31 +94,44 @@ const QuizTestPage = () => {
       )}
 
       <div className={`${showCountdown ? 'blur-lg' : ''}`}>
-        <QuizTimer onTimeUp={handleTimeUp} isCountdownFinished={isCountdownFinished} />
+        <div className="text-center mt-8">
+          <QuizTimer onTimeUp={handleTimeUp} isCountdownFinished={isCountdownFinished} />
+          <div className="max-w-[460px] my-4 items-center mx-auto text-center text-black text-xl font-semibold font-['Pretendard']">
+            인스타그램 스토리를 통해 @moaguide.official 태그 후 게시물을 공유하셨다면 인스타그램 아이디를 입력해주세요! <br/>(가점 제공 확인용)
+          </div>
 
-        <div className="mt-4">
-          <label>인스타그램 아이디</label>
-          <input
-            type="text"
-            value={insta}
-            onChange={(e) => setInsta(e.target.value)}
-            className="border p-2 w-full rounded"
-            placeholder="@username"
-          />
+          <div className="w-full max-w-md mx-auto">
+            <input
+              type="text"
+              value={insta}
+              onChange={(e) => setInsta(e.target.value)}
+              className="w-full max-w-[360px] px-4 py-[14px] bg-bg rounded-[12px] outline-none text-body2 focus:outline-normal"
+              placeholder="아이디 입력"
+            />
+          </div>
+
+          <div className="max-w-[350px] mt-4 mb-2 items-center mx-auto text-center text-black text-xl font-semibold font-['Pretendard']">
+            프리미엄 콘텐츠(학습하기)를 구독하셨다면 성함과 구독하신 아이디를 입력해주세요.
+          </div>
+
+          <div className="max-w-[350px] mb-4 items-center mx-auto text-center text-black text-sm font-base font-['Pretendard']">
+            (ex. 모아가이드 / moaguide@naver.com)
+          </div>
+
+          <div className="mt-2 w-full max-w-md mx-auto">
+            <input
+              type="text"
+              value={naver}
+              onChange={(e) => setNaver(e.target.value)}
+              className="w-full max-w-[360px] px-4 py-[14px] bg-bg rounded-[12px] outline-none text-body2 focus:outline-normal"
+              placeholder="성함/이메일 입력"
+            />
+          </div>
         </div>
 
-        <div className="mt-4">
-          <label>성함/이메일</label>
-          <input
-            type="text"
-            value={naver}
-            onChange={(e) => setNaver(e.target.value)}
-            className="border p-2 w-full rounded"
-            placeholder="이름/이메일"
-          />
+        <div className="mt-8 max-w-[600px] mx-auto">
+          <QuizQuestions questions={quizQuestions} onAnswerChange={handleAnswerChange} answers={answers} />
         </div>
-
-        <QuizQuestions questions={quizQuestions} onAnswerChange={handleAnswerChange} answers={answers} />
 
         <QuizSubmitButton onSubmit={submitQuiz} />
       </div>
