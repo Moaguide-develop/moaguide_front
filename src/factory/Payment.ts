cf93b5e979fed04d73b6c2651216d096e50cf858
eunhak <akfmzh1207@naver.com>
2024-12-22 22:42:07 +0900

    feat: 카드 삭제 api

--- src/factory/Payment.ts
+++ src/factory/Payment.ts
@@ -1,4 +1,5 @@
 import { axiosInstance } from '@/service/axiosInstance';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export const deletePayment = async () => {
   try {
@@ -8,4 +9,24 @@ export const deletePayment = async () => {
     console.error('Error fetching payment:', error);
     throw new Error('카드삭제에 실패했습니다.');
   }
+};
+
+export const useDeletePayment = () => {
+  const queryClient = useQueryClient();
+
+  const deletePaymentMutation = useMutation({
+    mutationFn: deletePayment,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['payments'] });
+
+      alert('카드가 삭제되었습니다.');
+      window.location.reload();
+    },
+    onError: (error) => {
+      console.error('Error deleting payment:', error);
+      alert('카드삭제에 실패했습니다.');
+    }
+  });
+
+  return { deletePaymentMutation };
 };
