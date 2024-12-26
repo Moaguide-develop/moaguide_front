cef197c70a1e7f8c167c9eebd729e99a01f5fc35
eunhak <akfmzh1207@naver.com>
2024-12-26 22:26:20 +0900

    setting: sentry 초기세팅

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,8 +1,20 @@
+import * as Sentry from '@sentry/nextjs';
+
 export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs' && process.env.NODE_ENV === 'development') {
-    const { server } = await import('./mocks/server');
-    server.listen({
-      onUnhandledRequest: 'bypass'
-    });
-  }
+  // if (process.env.NEXT_RUNTIME === 'nodejs' && process.env.NODE_ENV === 'development') {
+  //   const { server } = await import('./mocks/server');
+  //   server.listen({
+  //     onUnhandledRequest: 'bypass'
+  //   });
+  // }
+
+  // Sentry 초기화
+  const { init } = await import('@sentry/nextjs');
+
+  init({
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN, // Sentry DSN 환경 변수
+    tracesSampleRate: 1.0, // 성능 모니터링 비율
+    environment: process.env.NEXT_PUBLIC_SENTRY_ENV || 'development', // 환경 설정
+    integrations: [] // 필요한 Sentry 통합 옵션 추가
+  });
 }
