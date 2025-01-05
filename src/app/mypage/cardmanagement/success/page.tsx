e0161b9a37545c8d8186a506b45f6090f1be1b98
eunhak <akfmzh1207@naver.com>
2025-01-05 23:18:17 +0900

    fix: 카드 등록시 orderid 제거

--- src/app/mypage/cardmanagement/success/page.tsx
+++ src/app/mypage/cardmanagement/success/page.tsx
@@ -18,11 +18,11 @@ export default function CardRegisterSuccess() {
             <h2 className="text-3xl font-semibold text-normal">
               카드 등록에 성공했습니다.
             </h2>
-            <p className="text-gray-500 mt-4 font-semibold">등록 내역</p>
+            {/* <p className="text-gray-500 mt-4 font-semibold">등록 내역</p>
 
             <p className="text-body4  text-gray400 max-w-lg mx-auto mt-2">
               주문 ID: {orderId || ''}
-            </p>
+            </p> */}
             <div className="mt-8">
               <button
                 className="w-full bg-normal hover:shadow-lg text-white rounded-xl px-4 py-2.5"
