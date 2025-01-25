5771b0c361e1c7237218e22b8dd5a55133a1887c
jiohjung98 <ckaquz98@naver.com>
2025-01-25 16:27:14 +0900

    feat: related article router push

--- src/components/learning/SubscriptionBanner.tsx
+++ src/components/learning/SubscriptionBanner.tsx
@@ -1,6 +1,9 @@
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const SubscriptionBanner = () => {
+  const router = useRouter();
+
   return (
     <div className="w-full flex items-center justify-between bg-gradient-to-r from-gray-50 to-gray-100 rounded-2xl px-12 py-16 shadow-md max-w-[1000px] mx-auto mt-12">
       <div className="text-left">
@@ -10,7 +13,8 @@ const SubscriptionBanner = () => {
         </h2>
       </div>
       <div>
-        <button className="bg-gradient2 px-8 py-4 rounded-full shadow-md hover:bg-gradient2 text-neutral-50 text-lg font-bold font-['Inter'] transition duration-300 ">
+        <button className="bg-gradient2 px-8 py-4 rounded-full shadow-md hover:bg-gradient2 text-neutral-50 text-lg font-bold font-['Inter'] transition duration-300"
+          onClick={() => router.push('/payment')}>
           첫달 무료 구독하기
         </button>
       </div>
