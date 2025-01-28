1339e8b33fbddb6d22ae48e57fb0db9cd0e2bfcc
eunhak <akfmzh1207@naver.com>
2025-01-28 21:18:23 +0900

    feat : payment 로그인 분기처리

--- src/app/payment/layout.tsx
+++ src/app/payment/layout.tsx
@@ -1,13 +1,27 @@
-import { Suspense } from 'react';
+'use client';
+import { useAuthStore } from '@/store/userAuth.store';
+import { useRouter } from 'next/navigation';
+import { Suspense, useEffect } from 'react';
 
 export default function PaymentLayout({
   children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const router = useRouter();
+  const { isLoggedIn } = useAuthStore();
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      router.push('/sign');
+    }
+  }, [isLoggedIn, router]);
+
   return (
     <Suspense>
-      <div className="px-5 mt-5 w-full mx-auto sm:max-w-[640px] sm:mt-10 sm:px-0">{children}</div>
+      <div className="px-5 mt-5 w-full mx-auto sm:max-w-[640px] sm:mt-10 sm:px-0">
+        {children}
+      </div>
     </Suspense>
   );
 }
