2e9ef7c2e1c92d2a6fe0bb4c1b8bd66bac427ace
jiohjung98 <ckaquz98@naver.com>
2025-02-17 13:54:12 +0900

    feat: article page category image

---
+++ src/utils/categoryConfig.ts
@@ -0,0 +1,57 @@
+import NewsBackground from '../../public/images/learning/news_background.png';
+import GuideBackground from '../../public/images/learning/guide_background.png';
+import BuildingBackground from '../../public/images/learning/building_background.png';
+import ArtBackground from '../../public/images/learning/art_background.png';
+import MusicBackground from '../../public/images/learning/music_background.png';
+import ContentBackground from '../../public/images/learning/content_background.png';
+import CowBackground from '../../public/images/learning/cow_background.png';
+import MoneyBackground from '../../public/images/learning/money_background.png';
+import DefaultBackground from '../../public/images/learning/learning_background.png';
+
+export const categoryConfig = {
+  all: {
+    title: '학습하기',
+    backgroundImage: DefaultBackground,
+    description: '다양한 학습 콘텐츠를 탐색해보세요.',
+  },
+  news: {
+    title: '오늘의 뉴스',
+    backgroundImage: NewsBackground,
+    description: '오늘의 뉴스 소식을 확인해보세요',
+  },
+  guide: {
+    title: '조각투자 기초 가이드',
+    backgroundImage: GuideBackground,
+    description: '조각투자 기초 가이드 소식을 확인해보세요',
+  },
+  building: {
+    title: '부동산',
+    backgroundImage: BuildingBackground,
+    description: '부동산 소식을 확인해보세요',
+  },
+  art: {
+    title: '미술품',
+    backgroundImage: ArtBackground,
+    description: '미술품 소식을 확인해보세요',
+  },
+  music: {
+    title: '음악 저작권',
+    backgroundImage: MusicBackground,
+    description: '음악 저작권 소식을 확인해보세요',
+  },
+  content: {
+    title: '콘텐츠',
+    backgroundImage: ContentBackground,
+    description: '콘텐츠 소식을 확인해보세요',
+  },
+  cow: {
+    title: '한우',
+    backgroundImage: CowBackground,
+    description: '한우 소식을 확인해보세요',
+  },
+  money: {
+    title: '재테크 가이드',
+    backgroundImage: MoneyBackground,
+    description: '재테크 가이드 소식을 확인해보세요',
+  },
+};
